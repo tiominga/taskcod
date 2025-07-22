9ae94d9f90cd17d89418c4f0b95fea769c1463d8
tiominga <tiominga@gmail.com>
2025-07-22 18:52:01 -0300

    ter 22 jul 2025 18:52:01 -03 tristeza

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/accounts/login/')
+
 def home_index(request):
     return render(request, 'home/index.html')
 
