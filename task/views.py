551d9a08f16144664baebd30856e5b4b4a9a3c8f
tiominga <tiominga@gmail.com>
2025-07-25 21:45:25 -0300

    sex 25 jul 2025 21:45:25 -03 with validation

--- task/views.py
+++ task/views.py
@@ -2,30 +2,32 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Task
 from django.contrib import messages
-from django.http import JsonResponse
-import json
-
 
 @login_required
 def task_add(request):
     if request.method == 'POST':
         task = Task(
             cod_user=request.user.id,
-            id_clinica=request.POST['id_clinica'],
-            tipo=request.POST['tipo'],
-            prioridade=request.POST['prioridade'],
-            caminho=request.POST['caminho'],
-            descricao=request.POST['descricao']
+            id_clinica=request.POST.get('id_clinica'),
+            tipo=request.POST.get('tipo'),
+            prioridade=request.POST.get('prioridade'),
+            caminho=request.POST.get('caminho'),
+            descricao=request.POST.get('descricao')
         )
         try:
             task.full_clean()
             task.save()
-            data = {'success':True,'message':task.id}             
+            messages.success(request, 'Tarefa adicionada com sucesso!')
+            return redirect('print:print_form', cod_task=task.id)
         except Exception as e:
-            data = {'success':False,'message':e.messages[0]}             
-    return JsonResponse(data)      
-
-    
+            messages.error(request, e.messages[0])
+            # Volta para o form mostrando os dados preenchidos:
+            context = {
+                'task_data': request.POST,
+            }
+            return render(request, 'task_form.html', context)
+    else:
+        return render(request, 'task_form.html')
 
 @login_required
 def task_form(request):
